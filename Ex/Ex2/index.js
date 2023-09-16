f555da51d8836f58d4db826462eb05904d35b896
Bruno Da Silva <138171223+User3ds@users.noreply.github.com>
2023-09-16 17:15:56 +0200

    Blackjack project (WIP)
    1- Almost finished Blackjack project
    2- New challenges completed in Ex2

--- Ex/Ex2/index.js
+++ Ex/Ex2/index.js
@@ -8,12 +8,88 @@
 //     console.log("Welcome!")
 // }
 
-let age = 100;
+// let age = 100;
 
-if (age < 100) {
-    console.log("Not elegible")
-} else if (age === 100) {
-    console.log("Here is your birthday card from the king!")
-} else {
-    console.log("Not elegible, you've already gotten one")
+// if (age < 100) {
+//     console.log("Not elegible")
+// } else if (age === 100) {
+//     console.log("Here is your birthday card from the king!")
+// } else {
+//     console.log("Not elegible, you've already gotten one")
+// }
+
+ let techList = ["Javascript", "HTML5", "CSS3", "VueJs", "PHP", "SQL", "MariaDB", "Wordpress"]
+
+// for (let i = 0; i < techList.length; i++) {
+//     console.log(techList[i])
+// }
+
+ let newTech = "GitHub";
+
+techList.push(newTech) // Adds a new index to the end of the array
+
+// Both of this methods are mutating methods.
+techList.pop() // Deletes the LAST index of the array
+techList.shift() // Deletes the FIRST index of the arry
+
+// console.log(techList)
+
+// console.log( techList[1], techList[2], techList[0])
+
+// let me = ["Bruno", 31, true]
+
+
+
+// for (let i = 10; i < 101; i += 10) {
+//     console.log(i)
+// }
+
+// let cards = [3, 5, 6];
+
+// for (let i = 0; i < cards.length; i++) {
+//     console.log(cards[i])
+// }
+
+let sentence = ["Hello", "my", "name", "is", "Bruno"];
+let greetingEl = document.getElementById("greeting-el");
+
+for (let i = 0; i < sentence.length; i++) {
+    greetingEl.textContent += sentence[i] + " "
 }
+
+let player1Time = 102;
+let player2Time = 107;
+
+function getFastestRaceTime() {
+    if (player1Time < player2Time) {
+        return player1Time
+    } else if (player2Time < player1Time) {
+        return player2Time
+    } else {
+        return player1Time
+    }
+}
+
+let fastestTime = getFastestRaceTime()
+
+console.log(fastestTime)
+
+function getTotalRaceTime() {
+    let total = player1Time + player2Time 
+    return total 
+}
+
+let totalRaceTime = getTotalRaceTime();
+
+console.log(totalRaceTime)
+
+
+function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random() * 12) ;
+    return randomNumber
+}
+
+console.log(getRandomNumber())
+
+
+
--- Projects/Blackjack/index.js
+++ Projects/Blackjack/index.js
@@ -1,26 +1,66 @@
-"use strict"
+"use strict";
 
-let firstCard = 22;
-let secondCard = 1;
-
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message;
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? 🤔"
-    } else if (sum === 21) {
-        message = "Congratulations! You've got Blackjack 🤑" 
-        hasBlackjack = true;
+console.log(cards)
+
+// Function to get a random card value
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 14);
+    console.log(randomCard);
+    if (randomCard === 1) {
+        return 11;
+    } else if (randomCard > 10) {
+        return 10;
     } else {
-        message = "To bad, the game is over. 😭"
-        isAlive = false;
+        return randomCard;
     }
-    console.log(message)
 }
 
-console.log(isAlive)
+// Function to start the game
+function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards.push(firstCard, secondCard)
+    sum = firstCard + secondCard;
+    renderGame();  
+}
 
 
+
+// Function to render the game state
+function renderGame() {
+    cardsEl.textContent = "Your cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " | ";
+    }
+
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Congratulations! You've got Blackjack";
+        hasBlackjack = true;
+    } else {
+        message = "Too bad, the game is over.";
+        isAlive = false;
+    }
+    
+    messageEl.textContent = message;
+}
+
+// Function to draw a new card
+function getNewCard() {
+    let newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+}
